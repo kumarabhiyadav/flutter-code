5a5498af418c2b4dbf63bd7fab53a6c819afcacc
Abhinav Yadav <mr.kumarabhinavyadav@gmail.com>
2022-12-28 08:38:27 +0530

    first commit

---
+++ src/helper/classfication.ts
@@ -0,0 +1,16 @@
+// This Classifiaction file will help to check elements are in
+// Row || Coloum
+
+enum layoutType {
+  Coloum = "Coloum",
+  Row = "Row",
+}
+
+class Classifiaction {
+  frame: any;
+
+  constructor(frame: any) {
+    this.frame =  frame;
+  }
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    code: "./src/code.ts",
+    helper: "./src/helper/classfication.ts",
+    figma_properties: "./src/utils/figma_properties.ts",
+    flutter_properties: "./src/utils/flutter_properties.ts",
+    generator: "./src/utils/generator.ts",
+  },
+  output: {
+    filename: "[name].js",
+    path: __dirname+ "/dist",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+};
